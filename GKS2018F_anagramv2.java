6ad763a9cde8a63d55499fc5c362f5813448c70b
Kit Morgan <kit.j.morgan@gmail.com>
2022-06-18 23:08:48 -0400

    finished anagram

---
+++ GKS2018F_anagramv2.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GKS2018F_anagramv2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tests = Integer.parseInt(scanner.nextLine());
+        List<String[]> problems = new ArrayList<>();
+
+        for (int i = 0; i < tests; i++) {
+            scanner.nextLine();
+            String A = scanner.nextLine();
+            String B = scanner.nextLine();
+            String[] problem = {A, B};
+            problems.add(problem);
+        }
+
+        for (int i = 0; i < tests; i++) {
+            int answer = getSolution(problems.get(i));
+            System.out.printf("Case #%d: %d\n", i + 1, answer);
+        }
+
+    }
+
+    public static int getSolution(String[] problem) {
+        int counter = 0;
+        /** it worked but the wrong way so I flipped A and B */
+        String A = problem[1];
+        String B = problem[0];
+        List<String> substringsOfB = new ArrayList<>();
+        List<String> substringsOfA = new ArrayList<>();
+        //create list of substrings
+        for (int i = 0; i < B.length(); i++) {
+            for (int j = i + 1; j < B.length() + 1; j++) {
+                String pieceB = B.substring(i, j);
+                String pieceA = A.substring(i, j);
+                substringsOfB.add(pieceB);
+                substringsOfA.add(pieceA);
+            }
+        }
+
+        // iterate through our substring lists checking to see if piece A is contained in ANY of pieceB regardless of the order as long as sizes are the same
+        for (String pieceB : substringsOfB) {
+            char[] pieceBarray = pieceB.toCharArray();
+
+            for (String pieceA : substringsOfA) {
+                int matchingLetters = 0;
+                char[] pieceAarray = pieceA.toCharArray();
+                List<Character> pieceAList = new ArrayList<>();
+                List<Character> pieceBList = new ArrayList<>();
+                // check to see if sizes are equal
+                if (pieceAarray.length == pieceB.length()) {
+                    for (char chA : pieceAarray) {
+                        pieceAList.add(chA);
+                    }
+                    for (char chB : pieceBarray) {
+                        pieceBList.add(chB);
+                    }
+                    for(int aIndex = 0; aIndex < pieceAList.size(); aIndex++){
+                        for(int bIndex = 0; bIndex < pieceBList.size(); bIndex++){
+                            if(pieceAList.get(aIndex) == pieceBList.get(bIndex)){
+                                matchingLetters++;
+                                pieceBList.remove(bIndex);
+                                break;
+                            }
+                        }
+                    }
+                    if (matchingLetters == pieceB.length()){
+                        counter++;
+                        break;
+                    }
+                }
+
+            }
+        }
+        return counter;
+    }
+}
