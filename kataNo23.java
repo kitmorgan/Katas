38f3d1916f1b7de47da9eb69c581cf6f8f941c9e
Kit Morgan <kit.j.morgan@gmail.com>
2022-06-23 11:58:25 -0400

    tiny edit

--- kataNo23.java
+++ kataNo23.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class kataNo23 {
     public static void main(String[] args) {
-    int[] input = {1,2,3,4};
+        int[] input = {1, 2, 3, 4};
         System.out.println(no23(input));
     }
 
-    public static boolean no23(int[] input){
-        for(int i : input){
-            if(i == 2 || i == 3){
+    public static boolean no23(int[] input) {
+        for (int i : input) {
+            if (i == 2 || i == 3) {
                 return false;
             }
         }
