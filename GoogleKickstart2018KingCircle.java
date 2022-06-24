0afe3335f6ad85e50d8c62a8f1697e16cc4f182e
kitm <kit.j.morgan@gmail.com>
2022-06-24 18:44:42 -0400

    start king circle

---
+++ GoogleKickstart2018KingCircle.java
@@ -0,0 +1,11 @@
+public class GoogleKickstart2018KingCircle {
+    // a Royal Parade will march its way through the streets of the capital.
+    //The capital can be thought of as an infinite 2D plane, with infinitely many,
+    //infinitely long, streets spaced one meter apart, running horizontally and vertically throughout.
+    // The horizontal streets are labelled from negative infinity to infinity from top to bottom,
+    // while the vertical streets are labelled from negative infinity to infinity from left to right.
+    
+    //N, V1, H1, A, B, C, D, E, F and M
+    //Vi = (A × Vi-1 + B × Hi-1 + C) modulo M
+    //Hi = (D × Vi-1 + E × Hi-1 + F) modulo M
+}
